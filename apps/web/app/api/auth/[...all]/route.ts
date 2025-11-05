29e42b78b371d9b523ed564dd61b2c4b111d6276
Kan-A-Pesh <michel@impulselab.ai>
2025-11-05 16:40:28 +0100

    feat(ci): add CI workflow and enhance formatting checks in package.json

--- apps/web/app/api/auth/[...all]/route.ts
+++ apps/web/app/api/auth/[...all]/route.ts
@@ -1,4 +1,4 @@
 import { toNextJsHandler } from "better-auth/next-js";
-import { auth } from "database/auth/server";
+import { auth } from "@repo/database/auth/server";
 
 export const { POST, GET } = toNextJsHandler(auth);
--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,10 +1,11 @@
-import Image, { type ImageProps } from "next/image";
+import Image from "next/image";
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center justify-center h-screen">
       <section className="flex flex-col items-center justify-center gap-4">
+        <Image src="/next.svg" alt="Logo" width={197} height={80} />
         <h1 className="text-2xl font-bold">It works!</h1>
         <Button>Click me</Button>
       </section>
